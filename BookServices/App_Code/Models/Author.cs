e97394acedb4c25425393a13f6d09ccfff1062bc
Trong Tran <trongtqk68@gmail.com>
2017-11-11 01:46:00 +0700

    AddService

--- BookServices/App_Code/Models/Author.cs
+++ BookServices/App_Code/Models/Author.cs
@@ -7,9 +7,14 @@ namespace bookstore.Models
 {
     public class Author
     {
-        public string id_author { get; set; }
+        public int id_author { get; set; }
         public string name_author { get; set; }
         public string img_author { get; set; }
         public string description { get; set; }
+
+        //public string id { get; set; }
+        //public String name { get; set; }
+        //public String description { get; set; }
+        //public String img { get; set; }
     }
 }
